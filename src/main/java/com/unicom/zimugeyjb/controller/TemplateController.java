2818966fbf7666ab06b76a45f8d6525a64170eda
yib1091947832 <1091947832@qq.com>
2019-09-20 20:16:31 +0800

    5.7 thymeleaf内置对象与工具类

--- src/main/java/com/unicom/zimugeyjb/controller/TemplateController.java
+++ src/main/java/com/unicom/zimugeyjb/controller/TemplateController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Copyright: Unicom (Zhejiang) Industrial Internet Co., Ltd.    2019 <br/>
@@ -25,10 +28,21 @@ public class TemplateController {
     ArticleRestService articleRestService;
 
     @GetMapping("/thymeleaf")
-    public String thymeleaf(Model model){
+    public String thymeleaf(Model model, HttpSession session){
         List<ArticleVo> articleVos = articleRestService.getAll();
 
         model.addAttribute("articles",articleVos);
+
+        Map<String ,String > user = new HashMap<>();
+        user.put("id","1");
+        //user.put("username","yang");
+        user.put("username",null);
+        user.put("password","ayngjaibin");
+
+        model.addAttribute("user",user);
+
+        session.setAttribute("foo","dddddddd");
+
 
         //模板名称，实际的目录为 ：resources/templates/thymeleaftemp.html
         return "thymeleaftemp";
