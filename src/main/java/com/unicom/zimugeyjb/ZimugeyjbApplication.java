165df65e83a4318149aecd405733fcb73a88a9fa
yib1091947832 <1091947832@qq.com>
2019-09-29 20:00:51 +0800

    10.3.通过@Scheduled实现定时任务  完成

--- src/main/java/com/unicom/zimugeyjb/ZimugeyjbApplication.java
+++ src/main/java/com/unicom/zimugeyjb/ZimugeyjbApplication.java
@@ -4,12 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 /**mapper扫描的路径   mybatis*/
 @MapperScan(basePackages = {"com.unicom.zimugeyjb.dao"})
 /**    启动类开启异步任务   默认是不开启的   */
 @EnableAsync
+/**    开启定时任务   可以不放在这里
+ *    com.unicom.zimugeyjb.timertask.ScheduleConfig
+ * */
+//@EnableScheduling
 public class ZimugeyjbApplication {
 
     public static void main(String[] args) {
