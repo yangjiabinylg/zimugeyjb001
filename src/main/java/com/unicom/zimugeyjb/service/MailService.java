ecf9b8899bed751bea08eeaf7283a9e48637a831
yib1091947832 <1091947832@qq.com>
2019-10-07 21:03:10 +0800

    15章邮件全部完成

--- src/main/java/com/unicom/zimugeyjb/service/MailService.java
+++ src/main/java/com/unicom/zimugeyjb/service/MailService.java
@@ -1,11 +1,16 @@
 package com.unicom.zimugeyjb.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class MailService {
@@ -26,5 +31,65 @@ public class MailService {
         message.setText(content);
         mailSender.send(message);
     }
+
+
+    /**
+     * 发送html邮件
+     */
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+        //注意这里使用的是MimeMessage
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        //第二个参数是否是html，true
+        helper.setText(content, true);
+
+        mailSender.send(message);
+    }
+
+
+    /**
+     * 发送带附件的html邮件
+     */
+    public void sendAttchmentHtmlMail(String to, String subject, String content,String filePath) throws MessagingException {
+        //注意这里使用的是MimeMessage
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        //第二个参数是否是html，true
+        helper.setText(content, true);
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+        helper.addAttachment(fileName, file);
+
+        mailSender.send(message);
+    }
+
+
+
+    /**
+     * 发送带内联附件的html邮件
+     */
+    public void sendResourceAttchmentHtmlMail(String to, String subject, String content,String filePath) throws MessagingException {
+        //注意这里使用的是MimeMessage
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        //第二个参数是否是html，true
+        helper.setText(content, true);
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        helper.addInline("gaoyuanyuan", file);
+
+        mailSender.send(message);
+    }
+
 
 }
--- src/test/java/com/unicom/zimugeyjb/email/MailServiceTest.java
+++ src/test/java/com/unicom/zimugeyjb/email/MailServiceTest.java
@@ -1,12 +1,21 @@
 package com.unicom.zimugeyjb.email;
 
+import com.unicom.zimugeyjb.model.ArticleVo;
 import com.unicom.zimugeyjb.service.MailService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.*;
 
 
 @RunWith(SpringRunner.class)
@@ -19,4 +28,76 @@ public class MailServiceTest {
     public void sendSimpleMail() {
         mailService.sendSimpleMail("1091947832@qq.com","普通文本邮件","普通文本邮件内容测试");
     }
+
+
+    @Test
+    public void sendHtmlMail() throws MessagingException {
+        mailService.sendHtmlMail("1091947832@qq.com","一封html测试邮件","<body style=\"text-align: center;margin-left: auto;margin-right: auto;\">\n"
+                + " <div id=\"welcome\" style=\"text-align: center;position: absolute;\" >\n"
+                +"      <h3>\"一封html测试邮件\"</h3>\n"
+                +"      <span>http://www.zimug.com</span>"
+                + "     <div style=\"text-align: center; padding: 10px\"><a style=\"text-decoration: none;\" href=\"https://zimug.com\" target=\"_bank\" >"
+                + "           <strong>我很用心，希望你有所收获</strong></a></div>\n"
+                + " </div>\n" + "</body>");
+    }
+
+
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @Test
+    public void sendTemplateMail() throws IOException, TemplateException, MessagingException {
+        List<ArticleVo> articles = new ArrayList<>();
+        articles.add(new ArticleVo(1L,"zimug","手摸手教你学spring boot","内容一",new Date(),null));
+        articles.add(new ArticleVo(2L,"zimug","手摸手教你学spring boot","内容二",new Date(),null));
+
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate("fremarkertemp.html");
+
+        Map<String,Object> model = new HashMap<>();
+        model.put("articles",articles);
+        String templateHtml = FreeMarkerTemplateUtils.processTemplateIntoString(template,model);
+
+
+        mailService.sendHtmlMail("1091947832@qq.com","一封freemarker模板的html测试邮件",templateHtml);
+    }
+
+
+    @Test
+    public void sendAttchmentHtmlMail() throws IOException, TemplateException, MessagingException {
+
+        List<ArticleVo> articles = new ArrayList<>();
+        articles.add(new ArticleVo(1L,"zimug","手摸手教你学spring boot","内容一",new Date(),null));
+        articles.add(new ArticleVo(2L,"zimug","手摸手教你学spring boot","内容二",new Date(),null));
+
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate("fremarkertemp.html");
+
+        Map<String,Object> model = new HashMap<>();
+        model.put("articles",articles);
+        String templateHtml = FreeMarkerTemplateUtils.processTemplateIntoString(template,model);
+
+        String filePath = "D:\\template.png";
+
+        mailService.sendAttchmentHtmlMail("1091947832@qq.com","一封freemarker模板的html测试邮件",templateHtml,filePath);
+    }
+
+
+    @Test
+    public void sendResourceAttchmentHtmlMail() throws IOException, TemplateException, MessagingException {
+
+        List<ArticleVo> articles = new ArrayList<>();
+        articles.add(new ArticleVo(1L,"zimug","手摸手教你学spring boot","内容一",new Date(),null));
+        articles.add(new ArticleVo(2L,"zimug","手摸手教你学spring boot","内容二",new Date(),null));
+
+        Template template = freeMarkerConfigurer.getConfiguration().getTemplate("fremarkertemp.html");
+
+        Map<String,Object> model = new HashMap<>();
+        model.put("articles",articles);
+        String templateHtml = FreeMarkerTemplateUtils.processTemplateIntoString(template,model);
+
+        String filePath = "D:\\template.png";
+
+        mailService.sendResourceAttchmentHtmlMail("1091947832@qq.com","一封freemarker模板的html测试邮件",templateHtml,filePath);
+
+    }
+
 }
